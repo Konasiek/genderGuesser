39676282732148e3e8409c8d7373bae94e35e29b
Konasiek <konasiek@gmail.com>
2021-03-10 15:03:09 +0100

    initializing new project

---
+++ src/main/java/com/example/genderguesser/GenderGuesserApplication.java
@@ -0,0 +1,13 @@
+package com.example.genderguesser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenderGuesserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GenderGuesserApplication.class, args);
+    }
+
+}
